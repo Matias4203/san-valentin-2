ba01c75ab1f2e662882430ff25ca119a3a54b427
Matias4203 <ruequeti@gmail.com>
2025-02-05 00:24:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function mostrarMensaje() {
-    let mensaje = "💖 ¡Sabía que dirías que sí! Eres una persona increíble, y espero que hoy sea un día hermoso para ti. 🌹✨";
-    let nuevaVentana = window.open("", "_blank", "width=400,height=300");
-    nuevaVentana.document.write(`<h2 style="text-align:center;">${mensaje}</h2>`);
+    document.getElementById("modal").style.display = "block";
+}
+
+function cerrarModal() {
+    document.getElementById("modal").style.display = "none";
 }
