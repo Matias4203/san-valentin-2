adeaff6c8295ec657792f0869b9f949f5ea9dd08
Matias4203 <ruequeti@gmail.com>
2025-02-05 00:28:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function mostrarMensaje() {
 function cerrarModal() {
     document.getElementById("modal").style.display = "none";
 }
+
+function moverNo() {
+    var botonNo = document.getElementById("no");
+    var maxX = window.innerWidth - botonNo.offsetWidth;
+    var maxY = window.innerHeight - botonNo.offsetHeight;
+
+    var posX = Math.random() * maxX;
+    var posY = Math.random() * maxY;
+
+    botonNo.style.left = posX + "px";
+    botonNo.style.top = posY + "px";
+}
+
+function noSeleccionado() {
+    alert("¡Parece que preferiste no elegir el botón! Elige 'Sí' para ver el mensaje.");
+}
